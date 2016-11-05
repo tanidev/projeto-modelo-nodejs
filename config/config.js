9da43e12667f972026b9058165c5e62c3abd512a
Tani Araújo <tani.webdesigner@gmail.com>
2016-11-05 19:41:32 -0200

    Subida de versão

--- config/config.js
+++ config/config.js
@@ -27,6 +27,31 @@ var config = {
                 }
             }
         }
+    },
+    production: {
+        root: rootPath,
+        app: {
+            name: "ProjetoModelo"
+        },
+        port: 3000,
+        db: {
+            database: "d9r8qlficvfj98",
+            user: "glgqsblqunbbbs",
+            password: "4AK6BI5zqk9_s_7pOaG63eaBKl",
+            options: {
+                host: "ec2-54-235-254-56.compute-1.amazonaws.com:5432",
+                dialect: "postgres",
+                timezone: "America/Sao_Paulo",
+                define: {
+                  timestamps: false
+                },
+                pool: {
+                    max: 100,
+                    min: 0,
+                    idle: 10000
+                }
+            }
+        }
     }
 }
 
